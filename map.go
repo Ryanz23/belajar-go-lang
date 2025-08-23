6873477ba95d1a7c9e3a13dc60124ee5b0c89cd3
ryanz23 <ariyanandryan@gmail.com>
2025-08-23 14:08:35 +0700

    initialize map datatype program

---
+++ map.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	person := map[string]string{
+		"name":    "Ariyan Andryan",
+		"address": "Indonesia",
+	}
+
+	person["title"] = "Programmer"
+
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["address"])
+	fmt.Println(person["title"])
+
+	book := make(map[string]string)
+	book["title"] = "Belajar Go"
+	book["author"] = "Ariyan Andryan"
+	book["ups"] = "Salah"
+
+	fmt.Println(book)
+	fmt.Println(len(book))
+
+	delete(book, "ups")
+
+	fmt.Println(book)
+	fmt.Println(len(book))
+}
