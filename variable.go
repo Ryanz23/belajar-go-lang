c75e4b4c424cd77d6982df0efbf8ab14d7d0e7c9
ryanz23 <ariyanandryan@gmail.com>
2025-08-23 10:53:41 +0700

    initialize variable program

---
+++ variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Ariyan Andryan"
+	fmt.Println(name)
+
+	name = "Ariyan Aryja"
+	fmt.Println(name)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	age := 20
+	fmt.Println(age)
+
+	var (
+		firstName = "Ariyan Andryan"
+		lastName = "Aryja"
+	)
+
+	fmt.Println(firstName, lastName)
+}
